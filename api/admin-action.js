ae4be978691083e0b31a7580aa310dc600ece0ae
adiybukhori <muhammadadiy93@gmail.com>
2026-09-13 23:22:10 +0800

    Allow academic consultant admin actions

--- api/admin-action.js
+++ api/admin-action.js
@@ -3,6 +3,8 @@ const AUTH_WEB_APP = 'https://script.google.com/macros/s/AKfycbw22-UOsHkaap3dzU1
 
 const ALLOWED_ACTIONS = new Set([
   'v2ListWorkflow',
+  'v2UpsertAgent',
+  'v2AgentAdminStatus',
   'v2UpdateStage',
   'v2RunDocumentReview',
   'v2RunQualificationScreening',
